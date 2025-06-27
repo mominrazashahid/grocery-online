6c6ca8d2d9ae217d764b7ee696609741bf34dbb2
momin <mominrazashahid0@gmail.com>
2025-06-27 17:59:20 +0500

    test jenkins

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const users = [
   {
     id: 0,
     name: "Momin raza",
-    email:"momin@gmail.com",
+    email: "momin@gmail.com",
     address: "Okara Pakistan",
     gender: "Male",
   },
   {
     id: 1,
     name: "Ali raza",
-    email:"aliraza@gmail.com",
+    email: "aliraza@gmail.com",
     address: "Okara Pakistan",
     gender: "Male",
   },
@@ -41,30 +41,32 @@ app.get("/get-user/:id", (req, res) => {
 // create user
 app.post("/create-user", (req, res) => {
   const requestData = req.body;
-  console.log(requestData,'requestData');
+  console.log(requestData, "requestData");
   // validation
-  if(!requestData.name){
+  if (!requestData.name) {
     res.send("Name is required");
     return;
   }
 
-  if(!requestData.email){
+  if (!requestData.email) {
     res.send("Email is required");
-  }if(!requestData.address){
+  }
+  if (!requestData.address) {
     res.send("Address is required");
-  }if(!requestData.gender){
+  }
+  if (!requestData.gender) {
     res.send("Gender is required");
   }
 
   const user = users.find((user) => user.email == requestData.email);
-  if(user){
-    return  res.send("User with same email already exist");
+  if (user) {
+    return res.send("User with same email already exist");
   }
 
   let max = 0;
 
   users.forEach((targetUser) => {
-    if(targetUser.id > max){
+    if (targetUser.id > max) {
       max = targetUser.id;
     }
   });
@@ -72,8 +74,6 @@ app.post("/create-user", (req, res) => {
   requestData.id = max + 1;
   users.push(requestData);
   console.log(users);
-
-
 
   res.send("user Successfully created");
 });
